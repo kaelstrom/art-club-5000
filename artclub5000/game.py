b8acf195e0b81f5d36144fb0af1f6c29370fe0a4
Kyle Johnsen <kaelstrom@gmail.com>
2012-09-04 00:05:47 -0400

    left and right to cycle images, circles included

--- artclub5000/game.py
+++ artclub5000/game.py
@@ -6,11 +6,19 @@ def run_game():
 class Game(object):
     def __init__(self):
         self.objects = []
-        self.objects.append(gallerycompare.GalleryCompare())
-        self.objects[0].set_images(image.Image(), image.Image())
+        self.gc = gallerycompare.GalleryCompare()
         self.screen = pygame.display.set_mode((720,640))
         self.clock = pygame.time.Clock()
         self.running = True
+        self.images = []
+        for i in range(20):
+            self.images.append(image.Image())
+        self.image_a_index = 0
+        self.image_b_index = 1
+        self.gc.set_images(self.images[self.image_a_index], self.images[self.image_b_index])
+        self.actions = {}
+        self.actions['cycle image a'] = False
+        self.actions['cycle image b'] = False
         self.game_loop()
         
     def game_loop(self):
@@ -25,11 +33,24 @@ class Game(object):
             if event.type == pygame.QUIT:
                 self.running = False
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.actions['cycle image b'] = True
+                if event.key == pygame.K_RIGHT:
+                    self.actions['cycle image a'] = True
         
     def logic(self):
-        pass
+        if self.actions['cycle image a']:
+			self.actions['cycle image a'] = False
+			self.image_a_index = (self.image_a_index + 1) % len(self.images)
+			self.gc.set_images(self.images[self.image_a_index], self.images[self.image_b_index])
+        if self.actions['cycle image b']:
+			self.actions['cycle image b'] = False
+			self.image_b_index = (self.image_b_index + 1) % len(self.images)
+			self.gc.set_images(self.images[self.image_a_index], self.images[self.image_b_index])
     
     def draw(self):
+        self.gc.draw(self.screen)
         for e in self.objects:
             if e.draw:
                 e.draw(self.screen)
--- artclub5000/image.py
+++ artclub5000/image.py
@@ -7,8 +7,10 @@ class Image(object):
         self.surf.fill((50,0,0))
         self.rect = self.surf.get_rect()
         self.colors = [(200,0,0),(0,200,0),(0,0,200)]
-        self.stamp_size = 20
-        #print 'making a new image'
+        self.circles = 0
+        self.squares = 0
+        self.stamp_size = 40
+        print 'making a new image'
         self.randomize()
         
     def draw(self, screen):
@@ -17,10 +19,17 @@ class Image(object):
     def drawsquare(self, x, y, color=(255,255,255)):
         self.surf.fill(color, (x-self.stamp_size/2.0, y-self.stamp_size/2.0, self.stamp_size, self.stamp_size))
         
+    def drawcircle(self, x, y, color=(255,255,255)):
+        pygame.draw.circle(self.surf, color, (x,y), int(self.stamp_size/2.0))
+        
     def randomize(self):
         self.surf.fill((255,255,255))
         self.colors = []
         for i in range(3):
             self.colors.append([randint(0,255), randint(0,255), randint(0,255)])
-        for i in range(randint(6,18)):
+        for i in range(randint(0,20)):
+            self.squares += 1
             self.drawsquare(randint(0,240),randint(0,240), self.colors[randint(0,2)])
+        for i in range(randint(0,20)):
+            self.circles += 1
+            self.drawcircle(randint(0,240),randint(0,240), self.colors[randint(0,2)])
